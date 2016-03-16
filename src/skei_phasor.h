524206047d3e3c92f82efb7a94c13412e2e4abca
tor.helge.skei <tor.helge.skei@gmail.com>
2016-03-16 09:37:23 +0100

    renamed phase -> phasor

---
+++ src/skei_phasor.h
@@ -0,0 +1,64 @@
+#ifndef skei_phasor_included
+#define skei_phasor_included
+//----------------------------------------------------------------------
+
+// 0..1
+
+//#include "skei_math.h"
+
+//----------
+
+class SPhasor {
+
+  private:
+    float srate;
+
+  public:
+
+    float t,dt;
+
+  public:
+
+    SPhasor() {
+      t = 0;
+      dt = 0;
+      srate = 0;
+    }
+
+    void setSampleRate(float ASampleRate) {
+      srate = ASampleRate;
+    }
+
+    // AFrreq = hz
+
+    void setFreq(float AFreq, float ASampleRate) {
+      dt = AFreq / ASampleRate;
+    }
+
+    void addFreq(float AFreq, float ASampleRate) {
+      dt += AFreq / ASampleRate;
+    }
+
+    void setPhase(float APhase) {
+      t = 0;
+    }
+
+    /*
+      todo: negative frequency (t%1)
+    */
+
+    float process(void) {
+      t += dt;
+      // mod 1
+      if (t >= 1) t -= 1;
+
+      //t -= floor(t);
+      //while (p>=1) p -= 1;
+      //while (p<=0) p += 1;
+      return t;
+    }
+
+};
+
+//----------------------------------------------------------------------
+#endif
